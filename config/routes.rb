ac496ca26fda52e2572c2505eef73336b794ba9f
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-16 18:15:15 +0100

    routes defined for Board, Rental & User

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :boards, only: [:show, :index, :create, :new] do
+    resources :rentals, only: [:new, :create]
+  end
+
+  resources :rentals, only: [:show, :index, :edit, :update ]
+  resource :profile, only: [:show] # GET /profile profiles#show
+
 end
