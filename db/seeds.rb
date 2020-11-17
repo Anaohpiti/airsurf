38a8c7e3b04afa51fccac5150dad9add53e709ba
cameron brock <cameronb@usc.edu>
2020-11-17 14:58:45 +0100

    seed almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Board.destroy_all
+Rental.destroy_all
+
+user1 = User.new(
+  email:    "bill@example.com",
+  password: "password",
+  name: "bill"
+  )
+user1.photo.attach(io: File.open(Rails.root.join('db/fixtures/users/headshot_1.jpg')), filename: 'avatar_1.jpg')
+user1.save!
+
+user2 = User.new(
+  email:    "michel@example.com",
+  password: "password",
+  name: "mike"
+  )
+user2.photo.attach(io: File.open(Rails.root.join('db/fixtures/users/headshot_2.jpg')), filename: 'avatar_1.jpg')
+user2.save!
+
+board1 = User.new(
+  location: "Rennes",
+  height: 1.2,
+  volume: 33,
+  brand: "Lost",
+  condition: "good",
+  price_per_day: 15
+  )
+board1.photo.attach(io: File.open(Rails.root.join('db/fixtures/boards/surfboard_1.jpg')), filename: 'avatar_1.jpg')
+board1.save!
+
+board2 = User.new(
+    location: "St-Brieuc",
+    height: 1.85,
+    volume: 35.5,
+    brand: "Firewire",
+    condition: "Almost new",
+    price_per_day: 25
+  )
+board2.photo.attach(io: File.open(Rails.root.join('db/fixtures/boards/surfboard_2.jpg')), filename: 'avatar_1.jpg')
+board2.save!
+
+board1.user_id = user1
+board1.save
+board2.user_id = user2
+board2.save
+
+# rental1 = Rental.new (
+#   total_price: 50,
+#   start_date: Date.now,
+#   end_date: (Date.now + 1)
+# )
+# rental1.user_id = user1
+# rental1.board_id = board2
+
+# rental1.save
