408d3b98a7bedc7b6c5e641f7b3029f4dd398443
cameron brock <cameronb@usc.edu>
2020-11-17 16:15:43 +0100

    seed works with 2 users 2 borads and 1 rental

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 
 Board.destroy_all
 Rental.destroy_all
@@ -15,7 +16,7 @@ user1 = User.new(
   password: "password",
   name: "bill"
   )
-user1.photo.attach(io: File.open(Rails.root.join('db/fixtures/users/headshot_1.jpg')), filename: 'avatar_1.jpg')
+user1.photo.attach(io: File.open(Rails.root.join('db/fixtures/users/headshot_1.jpg')), filename: 'headshot_1.jpg')
 user1.save!
 
 user2 = User.new(
@@ -23,41 +24,45 @@ user2 = User.new(
   password: "password",
   name: "mike"
   )
-user2.photo.attach(io: File.open(Rails.root.join('db/fixtures/users/headshot_2.jpg')), filename: 'avatar_1.jpg')
+user2.photo.attach(io: File.open(Rails.root.join('db/fixtures/users/headshot_2.jpg')), filename: 'headshot_2.jpg')
 user2.save!
 
 board1 = Board.new(
   location: "Rennes",
-  height: 1.2,
+  description: "cool board for beginners",
+  title: "Soft Board 8 ft",
+  height: 12,
   volume: 33,
   brand: "Lost",
   condition: "good",
-  price_per_day: 15
+  price_per_day: 1500
   )
-board1.photo.attach(io: File.open(Rails.root.join('db/fixtures/boards/surfboard_1.jpg')), filename: 'avatar_1.jpg')
 board1.user = user1
-board1.save!
+board1.photo.attach(io: File.open(Rails.root.join('db/fixtures/boards/surfboard_1.jpg')), filename: 'surfboard_1.jpg')
+board1.save
 
 board2 = Board.new(
     location: "St-Brieuc",
-    height: 1.85,
-    volume: 35.5,
+    description: "for advanced surfers",
+    title: "Performance board thruster setup",
+    height: 185,
+    volume: 3555,
     brand: "Firewire",
     condition: "Almost new",
-    price_per_day: 25
+    price_per_day: 2500
   )
-board2.photo.attach(io: File.open(Rails.root.join('db/fixtures/boards/surfboard_2.jpg')), filename: 'avatar_1.jpg')
+board2.photo.attach(io: File.open(Rails.root.join('db/fixtures/boards/surfboard_2.jpg')), filename: 'surfboard_2.jpg')
 board2.user = user2
-board2.save!
+board2.save
 
 
 
-# rental1 = Rental.new (
-#   total_price: 50,
-#   start_date: Date.now,
-#   end_date: (Date.now + 1)
-# )
-# rental1.user_id = user1
-# rental1.board_id = board2
+rental1 = Rental.new(
+  total_price: 50,
+  start_date: Time.now,
+  end_date: (Time.now + 240000)
+)
+rental1.user = user1
+rental1.board = board2
 
-# rental1.save
+rental1.save
