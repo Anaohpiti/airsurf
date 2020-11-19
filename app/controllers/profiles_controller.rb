f22aa6b884f0fdd39ca54ac82e40026184a5c9bf
cameron brock <cameronb@usc.edu>
2020-11-19 13:11:01 +0100

    approve works and tabs work

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,11 +3,22 @@ before_action(:set_profile, only: [:show])
 
 # profile   GET  /profile(.:format)   profiles#show
   def show
+    @owner_has_no_rentals = owner_has_no_rentals?
   end
 
   private
 
   def set_profile
     @profile = current_user
+  end
+
+  def owner_has_no_rentals?
+    count = 0
+      @profile.boards.each do |board|
+        board.rentals.each do |rental|
+          count += 1 if rental
+        end
+      end
+    return count == 0
   end
 end
