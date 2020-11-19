1c30141c7220d9ae369417c2ced3640a49b957ce
Pierre Bergue <pierre.bergue@gmail.com>
2020-11-19 12:56:26 +0100

    delete the scrolling on registration pages, add sign up button, change design of buttons, deleted for reel the margin on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,5 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only:[:home]
-  before_action :home_page, only: [:home]
   def home
-  end
-
-  private
-
-  def home_page
-    @disable_footer = true
   end
 end
