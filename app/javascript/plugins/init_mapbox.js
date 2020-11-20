c59d2d82757c8188faf7d18d6f0868c2114de0b3
Pierre Bergue <pierre.bergue@gmail.com>
2020-11-20 13:28:11 +0100

    improved the map style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/pbergue/ckhq6vd2q0d4i1apdc85ssza4'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
